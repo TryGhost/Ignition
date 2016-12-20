f485a9bdfec7d3b03e503c86d12247b04b02a421
kirrg001 <katharina.irrgang@googlemail.com>
2016-12-21 00:30:13 +0100

    ✨  loggly stream: add option to match certain logs
    - you can add a match property, which contains a regex as string to match only certain logs to transmit to loggly
    - example: level:critical or statusCode:500|statusCode:301 or ^((?!statusCode:4\\d{2}).)*$ (match all entries where status code is not 4xx)
    - add json stringify safe dependency

--- test/logging.test.js
+++ test/logging.test.js
@@ -1,7 +1,9 @@
 var PrettyStream = require('../lib/logging/PrettyStream');
 var GhostLogger = require('../lib/logging/GhostLogger');
+var errors = require('../lib/errors');
 var sinon = require('sinon');
 var should = require('should');
+var Bunyan2Loggly = require('bunyan-loggly');
 var sandbox = sinon.sandbox.create();
 
 describe('Logging', function () {
@@ -35,10 +37,174 @@ describe('Logging', function () {
 
         var ghostLogger = new GhostLogger();
 
-        ghostLogger.info({
+        ghostLogger.error({
             err: new Error('message'),
             req: {body: {password: '12345678', data: {attributes: {pin: '1234', test: 'ja'}}}},
             res: {headers: {}}
         });
+    });
+
+    it('loggly does only stream certain errors', function (done) {
+        sandbox.stub(Bunyan2Loggly.prototype, 'write', function (data) {
+            should.exist(data.err);
+            done();
+        });
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: 'level:critical'
+            }
+        });
+
+        ghostLogger.error(new errors.InternalServerError());
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
+    });
+
+    it('loggly does only stream certain errors', function () {
+        sandbox.spy(Bunyan2Loggly.prototype, 'write');
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: 'level:critical'
+            }
+        });
+
+        ghostLogger.error(new errors.NotFoundError());
+        Bunyan2Loggly.prototype.write.called.should.eql(false);
+    });
+
+    it('loggly does only stream certain errors', function () {
+        sandbox.spy(Bunyan2Loggly.prototype, 'write');
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: '^((?!statusCode:4\\d{2}).)*$'
+            }
+        });
+
+        ghostLogger.error(new errors.NotFoundError());
+        Bunyan2Loggly.prototype.write.called.should.eql(false);
+    });
+
+    it('loggly does only stream certain errors', function () {
+        sandbox.spy(Bunyan2Loggly.prototype, 'write');
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: '^((?!statusCode:4\\d{2}).)*$'
+            }
+        });
+
+        ghostLogger.error(new errors.NoPermissionError());
+        Bunyan2Loggly.prototype.write.called.should.eql(false);
+    });
+
+    it('loggly does only stream certain errors', function () {
+        sandbox.spy(Bunyan2Loggly.prototype, 'write');
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: '^((?!statusCode:4\\d{2}).)*$'
+            }
+        });
+
+        ghostLogger.error(new errors.InternalServerError());
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
+    });
+
+    it('loggly does only stream certain errors', function (done) {
+        sandbox.stub(Bunyan2Loggly.prototype, 'write', function (data) {
+            should.exist(data.err);
+            done();
+        });
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: 'level:normal'
+            }
+        });
+
+        ghostLogger.error(new errors.NotFoundError());
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
+    });
+
+    it('loggly does only stream certain errors', function (done) {
+        sandbox.stub(Bunyan2Loggly.prototype, 'write', function (data) {
+            should.exist(data.err);
+            done();
+        });
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: 'level:critical|statusCode:404'
+            }
+        });
+
+        ghostLogger.error(new errors.NotFoundError());
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
+    });
+
+    it('loggly does only stream certain errors', function (done) {
+        sandbox.stub(Bunyan2Loggly.prototype, 'write', function (data) {
+            should.exist(data.err);
+            should.exist(data.req);
+            should.exist(data.res);
+            done();
+        });
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid',
+                match: 'statusCode:404'
+            }
+        });
+
+        ghostLogger.error({
+            err: new errors.NotFoundError(),
+            req: {body: {password: '12345678', data: {attributes: {pin: '1234', test: 'ja'}}}},
+            res: {headers: {}}
+        });
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
+    });
+
+    it('loggly does only stream certain errors: match is not defined -> log everything', function (done) {
+        sandbox.stub(Bunyan2Loggly.prototype, 'write', function (data) {
+            should.exist(data.err);
+            done();
+        });
+
+        var ghostLogger = new GhostLogger({
+            transports: ['loggly'],
+            loggly: {
+                token: 'invalid',
+                subdomain: 'invalid'
+            }
+        });
+
+        ghostLogger.error(new errors.NotFoundError());
+        Bunyan2Loggly.prototype.write.called.should.eql(true);
     });
 });
